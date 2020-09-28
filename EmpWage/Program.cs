e2cf81d147639c640c61139f8cfe250d624756c7
N1vas <ggj.nivas.18@gmail.com>
2020-09-28 23:02:00 +0530

    [N1vas] Refactor : UC6 condition added on working hours

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -8,6 +8,7 @@ namespace EmpWage
         public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_OF_WORKING_DAYS = 2;
+        public const int MAX_HRS_IN_MONTH = 10;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Problem on Master Branch");
@@ -15,10 +16,13 @@ namespace EmpWage
             int empHrs = 0;
             int empWage = 0;
             int totalEmpWage = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
             Random random = new Random();
-            for (int i= 0; i < NUM_OF_WORKING_DAYS; i++)
+            while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
             {
                 int empCheck = random.Next(0, 3);
+                totalWorkingDays++;
                 switch (empCheck)
                 {
                     case 2:
@@ -32,11 +36,12 @@ namespace EmpWage
                         break;
                 }
 
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
-                Console.WriteLine("Employee Wage :" + empWage);
-                Console.ReadKey();
+                //empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day:"+  totalWorkingDays+" Emp hrs "+empHrs );
+                //Console.ReadKey();
             }
+             totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Total Emp Wage : "+totalEmpWage);
         }
     }
