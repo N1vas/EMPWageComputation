a5a734238fc9b6a923a5d93b7d8384cce7bcaf81
N1vas <ggj.nivas.18@gmail.com>
2020-09-30 08:16:53 +0530

    [N1vas] Refactor : UC8 Employee wage for multi companies

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -5,19 +5,14 @@ namespace EmpWage
     {
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 2;
-        public const int MAX_HRS_IN_MONTH = 10;
-        public static int computeEmpWage()
+        public static int computeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Problem on Master Branch");
             int empHrs = 0;
-            int empWage = 0;
-            int totalEmpWage = 0;
             int totalEmpHrs = 0;
             int totalWorkingDays = 0;
             Random random = new Random();
-            while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
+            while(totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
             {
                 int empCheck = random.Next(0, 3);
                 totalWorkingDays++;
@@ -36,13 +31,14 @@ namespace EmpWage
                 totalEmpHrs += empHrs;
                 Console.WriteLine("Day:"+  totalWorkingDays+" Emp hrs "+empHrs );
             }
-             totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            int totalEmpWage = totalEmpHrs * empRatePerHour;
             Console.WriteLine("Total Emp Wage : "+totalEmpWage);
             return totalEmpWage;
         }
         static void Main(string[] args)
         {
-            computeEmpWage();
+            computeEmpWage("DMart", 20, 2, 10);
+            computeEmpWage("Reliance", 10, 4, 20);
         }
     }
 }
